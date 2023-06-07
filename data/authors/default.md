80f9371fbefaac5b904b8119713f688a69c465fc
ChrisWang <841531691@qq.com>
2023-06-07 12:16:30 +0800

    doc:更新站点信息

--- data/authors/default.md
+++ data/authors/default.md
@@ -2,13 +2,12 @@
 name: ChrisCoder
 avatar: /static/images/avatar.png
 occupation: 前端工程师
-company: 分享 JavaScript 热门框架，记录前端工程师学习成长历程。
-email: maqi1520@163.com
-juejin: https://juejin.cn/user/2189882895384093
+company: 分享资讯，互帮互助。
+email: 841531691@qq.com
 zhihu: https://www.zhihu.com/people/xiao-ma-15-3
-github: https://github.com/maqi1520
+github: https://github.com/SilverComet7
 ---
 
 - 姓名：ChrisWang
 - 昵称：ChrisCoder
-- 我是一名前端工程师，非典型程序员，探索游戏，副业增收
+- 我是一名前端工程师，非典型程序员，游戏热爱者，探索软件资源，副业增收等领域
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,16 +6,14 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://maqib.cn',
-  siteRepo: 'https://github.com/maqi1520/nextjs-tailwind-blog',
+  siteRepo: 'https://github.com/SilverComet7/nextjs-tailwind-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'maqi1520@163.com',
-  github: 'https://github.com/maqi1520',
-  juejin: 'https://juejin.cn/user/2189882895384093',
-  zhihu: 'https://www.zhihu.com/people/xiao-ma-15-3',
+  github: 'https://github.com/SilverComet7',
   locale: 'zh-CN',
-  welcome: '丰富的资讯',
+  welcome: '你所需要的资讯',
   analytics: {
     //如果要使用分析提供程序，则必须将其添加到。
     //`next.config.js`文件中的内容安全策略。
