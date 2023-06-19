0187bab58fbee268eac406d022283c7a51e4924a
ChrisWang <841531691@qq.com>
2023-06-19 17:32:17 +0800

    fix:调整信息

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -29,12 +29,6 @@ const projectsData = [
     imgSrc: '/static/images/lowcode.png',
     href: 'https://low-code.runjs.cool/',
   },
-  {
-    title: 'vscode-fanyi',
-    description: `vscode 翻译插件`,
-    imgSrc: '/static/images/vscode.png',
-    href: 'https://marketplace.visualstudio.com/items?itemName=maqi1520.everest-fanyi',
-  },
 ]
 
 export default projectsData
